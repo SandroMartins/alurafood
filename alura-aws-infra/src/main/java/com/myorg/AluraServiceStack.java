24b5020c65a776a33795b57fbc343a0470e6dc54
Sandro Martins <sandro.ireno@gmail.com>
2024-12-19 00:02:57 -0300

    commit projeto curso de microsservicos

---
+++ alura-aws-infra/src/main/java/com/myorg/AluraServiceStack.java
@@ -0,0 +1,40 @@
+package com.myorg;
+
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.HealthCheck;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.constructs.Construct;
+
+public class AluraServiceStack extends Stack {
+    public AluraServiceStack(final Construct scope, final String id, final Cluster cluster) {
+        this(scope, id, null, cluster);
+    }
+
+    public AluraServiceStack(final Construct scope, final String id, final StackProps props, final Cluster cluster) {
+        super(scope, id, props);
+
+        ApplicationLoadBalancedFargateService.Builder.create(this, "AluraService")
+                .serviceName("alura-service-teste")
+                .cluster(cluster)           // Required;
+                .cpu(512)                   // Default is 256
+                .desiredCount(1)            // Default is 1
+                .listenerPort(8080)
+                .assignPublicIp(true)
+                .taskImageOptions(
+                        ApplicationLoadBalancedTaskImageOptions.builder()
+                                .image(ContainerImage.fromRegistry("amazon/amazon-ecs-sample"))
+                                .containerPort(8080)
+                                .containerName("app_teste")
+                                .build())
+                .memoryLimitMiB(1024)       // Default is 512
+                .publicLoadBalancer(true)  // Default is false
+                .build();
+
+
+
+    }
+}
